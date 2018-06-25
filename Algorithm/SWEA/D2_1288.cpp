50064f91ae326cdee18334e5136c0fea030b9cda
sangchul <1ilsang@naver.com>
2018-06-25 22:17:23 +0900

    1288

--- Algorithm/SWEA/D2_1288.cpp
+++ Algorithm/SWEA/D2_1288.cpp
@@ -1,48 +1,50 @@
 ////https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV18_yw6I9MCFAZN&categoryId=AV18_yw6I9MCFAZN&categoryType=CODE
-//
-//#include <istream>
-//#include <iostream>
-//#include <string>
-//#include <algorithm>
-//#include <vector>
-//#include <utility>
-//#include <math.h>
-//
-//using namespace std;
-//pair<int, int> home, work;
-//vector<pair<int, int>> v;
-//
-//int ans;
-//int check[10];
-//int chk;
-//
-//void go(int n, int cnt){
-//    int tmp = n;
-//    while(tmp){
-//        int mod = tmp % 10;
-//        check[mod] = 1;
-//        chk++;
-//        tmp /= 10;
-//        printf("1");
-//    }
-//    if(chk == 9){
-//        ans = cnt;
-//        return;
-//    }
-//    else {
-//        go(n * 2, cnt + 1);
-//    }
-//}
-//
-//int main(void) {
-//    int t, cnt = 1;
-//    scanf("%d", &t);
-//
-//    while (t--) {
-//        int n;
-//        scanf("%d", &n);
-//        go(n, 0);
-//        printf("#%d %d\n", cnt++, ans);
-//    }
-//    return 0;
-//}
+
+#include <istream>
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <math.h>
+
+using namespace std;
+
+int ans;
+int check[10];
+int chk = 0;
+int m;
+
+void go(int n, int cnt){
+    int tmp = n;
+    while(tmp){
+        int mod = tmp % 10;
+		if (check[mod] == 0) {
+			check[mod] = 1;
+			chk++;
+//			printf("%d %d %d\n", n, mod, cnt);
+		}
+        tmp /= 10;
+    }
+    if(chk == 10){
+        ans = cnt + 1;
+        return;
+    }
+    else {
+        go(m * (cnt + 2), cnt + 1);
+    }
+}
+
+int main(void) {
+    int t, cnt = 1;
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d", &m);
+        go(m, 0);
+        printf("#%d %d\n", cnt++, ans * m);
+		memset(check, 0, sizeof(check));
+		chk = 0;
+    }
+    return 0;
+}
