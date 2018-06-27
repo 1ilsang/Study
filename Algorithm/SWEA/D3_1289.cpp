81622eb045f354fda97a507f888c66e0d0db5ab7
LEE SANG-CHUL <1ilsang@naver.com>
2018-06-27 14:13:11 +0900

    plo

---
+++ Algorithm/SWEA/D3_1289.cpp
@@ -0,0 +1,19 @@
+//
+// Created by user on 2018. 6. 27..
+//
+
+//https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV19AcoKI9sCFAZN&categoryId=AV19AcoKI9sCFAZN&categoryType=CODE
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main(void){
+    int t, cnt = 1;
+    scanf("%d", &t);
+    while (t--){
+        int v;
+        scanf("%d", &v);
+        printf("%d\n", v);
+    }
+}
