31168f380b9de62d50b083624d623a0a3aecad75
sangchul <1ilsang@naver.com>
2018-07-06 16:48:56 +0900

    D4_1486

---
+++ Algorithm/SWEA/D4_1486.cpp
@@ -0,0 +1,33 @@
+//https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2b7Yf6ABcBBASw&categoryId=AV2b7Yf6ABcBBASw&categoryType=CODE
+
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int arr[21];
+int check[21];
+int ans = 22000000, n, b;
+
+void go(int now, int nowSum) {
+	if (now > n) return;
+	if (nowSum >= b) {
+		ans = min(ans, nowSum);
+	}
+	go(now + 1, nowSum + arr[now]);
+	go(now + 1, nowSum);
+}
+
+int main(void) {
+	int t, cnt = 1;
+	scanf("%d", &t);
+	while (t--) {
+		scanf("%d %d", &n, &b);
+		for (int i = 0; i < n; i++) {
+			scanf("%d", &arr[i]);
+		}
+		go(0, 0);
+		printf("#%d %d\n", cnt++, ans - b);
+		ans = 22222222;
+	}
+	return 0;
+}
