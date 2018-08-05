c1f6e44737cb1616c08796d8e527160bc2000f63
LEE SANG-CHUL <1ilsang@naver.com>
2018-08-05 21:45:01 +0900

    Create 2636.cpp

---
+++ Algorithm/BOJ/2636.cpp
@@ -0,0 +1,74 @@
+//https://www.acmicpc.net/problem/2636
+//
+
+#include <cstdio>
+#include <cstring>
+
+int dr[4] = { 0,1,0,-1 };
+int dc[4] = { 1,0,-1,0 };
+int ch[101][101];
+int check[101][101];
+int board[101][101];
+int r, c;
+
+int dfs(int nowR, int nowC) {
+	int ret = 1;
+	check[nowR][nowC] = 1;
+	for (int i = 0; i < 4; i++) {
+		int nr = nowR + dr[i];
+		int nc = nowC + dc[i];
+		if (0 <= nr && 0 <= nc && nr < r && nc < c
+			&& board[nr][nc] == 1 && check[nr][nc] == 0) {
+			ret += dfs(nr, nc);
+		}
+	}	
+	return ret;
+}
+void dfs2(int nowR, int nowC) {
+	check[nowR][nowC] = -1;
+	for (int i = 0; i < 4; i++) {
+		int nr = nowR + dr[i];
+		int nc = nowC + dc[i];
+		if (0 <= nr && 0 <= nc && nr < r && nc < c) {
+			if (board[nr][nc] == 1) {
+				ch[nr][nc] = 1;
+			}
+			if (board[nr][nc] == 0 && check[nr][nc] == 0)
+				dfs2(nr, nc);
+		}
+	}
+}
+int main(void) {
+	int k = 1;
+	scanf("%d %d", &r, &c);
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			scanf("%d", &board[i][j]);
+		}
+	}
+	while (k++) {
+		int che1 = 0, che2 = 0;
+		dfs2(0, 0);
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				if(board[i][j] == 1 && check[i][j] == 0)
+					che1 += dfs(i, j);
+			}
+		}
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				if (board[i][j] == ch[i][j] && ch[i][j] == 1) {
+					che2++;
+					board[i][j] = 0;
+				}
+			}
+		}
+		if (che1 == che2) {
+			printf("%d\n%d\n", k - 1, che1);
+			break;
+		}
+		memset(ch, 0, sizeof(ch));
+		memset(check, 0, sizeof(check));
+	}
+	return 0;
+}
