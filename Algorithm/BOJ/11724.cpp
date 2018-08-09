3105423c770f89d425884846db61e2c37624b277
LEE SANG-CHUL <1ilsang@naver.com>
2018-08-09 17:56:16 +0900

    Create 11724.cpp

---
+++ Algorithm/BOJ/11724.cpp
@@ -0,0 +1,36 @@
+//https://www.acmicpc.net/problem/11724
+//
+
+#include <cstdio>
+#include <vector>
+using namespace std;
+vector<vector<int>> v;
+int check[1001];
+
+void go(int now) {
+	check[now] = 1;
+	for (int i = 0; i < v[now].size(); i++) {
+		int next = v[now][i];
+		if(check[next] == 0) go(next);
+	}
+}
+
+int main(void) {
+	int n, m, ans = 0;
+	scanf("%d %d", &n, &m);
+	v.resize(n + 1);
+	for (int i = 0; i < m; i++) {
+		int u, w;
+		scanf("%d %d", &u, &w);
+		v[u].push_back(w);
+		v[w].push_back(u);
+	}
+	for (int i = 1; i <= n; i++){
+		if (check[i] == 0) {
+			ans++;
+			go(i);
+		}
+	}
+	printf("%d\n", ans);
+	return 0;
+}
