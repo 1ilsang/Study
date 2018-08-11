91b7759e62d9256d1182da5b1d6488d408444b4f
LEE SANG-CHUL <1ilsang@naver.com>
2018-08-11 15:29:46 +0900

    BOJ 11718 Simulation
    입력 그대로 출력하기

---
+++ Algorithm/BOJ/11718.cpp
@@ -0,0 +1,13 @@
+//https://www.acmicpc.net/problem/11718
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+	string str;
+	while (!getline(cin, str).eof()) {
+		cout << str << endl;
+	}
+	return 0;
+}
