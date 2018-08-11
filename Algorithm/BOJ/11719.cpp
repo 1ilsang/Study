499715963b40a40743ee8e09a3bef037a8429dc5
LEE SANG-CHUL <1ilsang@naver.com>
2018-08-11 15:30:25 +0900

    Create 11719.cpp

---
+++ Algorithm/BOJ/11719.cpp
@@ -0,0 +1,14 @@
+//https://www.acmicpc.net/problem/11719
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+	string str;
+	while (!getline(cin, str).eof()) {
+		cout << str << endl;
+	}
+	return 0;
+}
