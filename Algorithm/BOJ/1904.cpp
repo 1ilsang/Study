5a47a71b6b75ff16ca80bb0a68791e201f17849c
LEE SANG-CHUL <1ilsang@naver.com>
2018-08-14 12:53:54 +0900

    Create 1904.cpp

---
+++ Algorithm/BOJ/1904.cpp
@@ -0,0 +1,18 @@
+//https://www.acmicpc.net/problem/1904
+//
+
+#include <cstdio>
+
+int d[1000001];
+
+int main() {
+	int n;
+	d[1] = 1;
+	d[2] = 2;
+	scanf("%d", &n);
+	for (int i = 3; i <= n; i++) {
+		d[i] = (d[i - 1] + d[i - 2]) % 15746;
+	}
+	printf("%d\n", d[n]);
+	return 0;
+}
