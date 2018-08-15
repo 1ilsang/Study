aa67430e98463d8170d3cac2d1be896ef40eb159
LEE SANG-CHUL <1ilsang@naver.com>
2018-08-15 16:07:03 +0900

    Create 2193.cpp

---
+++ Algorithm/BOJ/2193.cpp
@@ -0,0 +1,17 @@
+//https://www.acmicpc.net/problem/2193
+//
+
+#include <iostream>
+using namespace std;
+long long d[91];
+int main() {
+	int n;
+	cin >> n;
+	d[1] = 1;
+	d[2] = 1;
+	for (int i = 3; i <= n; i++) {
+		d[i] = d[i - 1] + d[i-2];
+	}
+	cout << d[n] << endl;
+	return 0;
+}
