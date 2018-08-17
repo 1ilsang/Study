d065185626f2918ee89413d703f41a35db859a99
LEE SANG-CHUL <1ilsang@naver.com>
2018-08-17 17:35:36 +0900

    BOJ 2293 dp

---
+++ Algorithm/BOJ/2293.cpp
@@ -0,0 +1,25 @@
+//https://www.acmicpc.net/problem/2293
+//
+
+#include <cstdio>
+
+int coin[101];
+int dp[10001];
+
+int main()
+{
+	int n, k;
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++) scanf("%d", &coin[i]);
+	dp[0] = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (j - coin[i] >= 0) {
+				dp[j] += dp[j - coin[i]];
+				//	printf("%d %d | %d %d\n", i, j, j - coin[i], dp[j]);
+			}
+		}
+	}
+	printf("%d\n", dp[k]);
+	return 0;
+}
