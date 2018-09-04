e238103f25d641207434eb48974136b3cc430f5b
LEE SANG-CHUL <1ilsang@naver.com>
2018-09-04 21:49:45 +0900

    Create 10951.cpp

---
+++ Algorithm/BOJ/10951.cpp
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/10951
+//
+
+#include <cstdio>
+
+int main(void) {
+	int a, b;
+	while (scanf("%d %d", &a, &b) != EOF) {
+		printf("%d\n", a + b);
+	}
+	return 0;
+}
