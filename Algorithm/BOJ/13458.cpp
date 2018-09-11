c2e42f1a3c0f3bdbeda6f56c56011d8d524756e5
LEE SANG-CHUL <1ilsang@naver.com>
2018-09-11 16:08:37 +0900

    Create 13458.cpp

---
+++ Algorithm/BOJ/13458.cpp
@@ -0,0 +1,30 @@
+//https://www.acmicpc.net/problem/13458
+//
+
+#include <cstdio>
+
+long long arr[1000001];
+long long n, b, c;
+long long ans;
+
+int main(void) {
+	scanf("%lld", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%lld", &arr[i]);
+	}
+	scanf("%lld %lld", &b, &c);
+	for (int i = 0; i < n; i++) {
+		if (arr[i] == 0) continue;
+		ans++;
+		arr[i] = arr[i] - b;
+		if (arr[i] <= 0) arr[i] = 0;
+		else if(arr[i] % c == 0) {
+			ans += (arr[i] / c);
+		}
+		else if (arr[i] % c > 0) {
+			ans += (arr[i] / c + 1);
+		}
+	}
+	printf("%lld\n", ans);
+	return 0;
+}
