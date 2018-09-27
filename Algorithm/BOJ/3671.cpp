a97c39086d94ec7eda601bd0dc4790c50efa5c15
LEE SANG-CHUL <1ilsang@naver.com>
2018-09-28 02:53:57 +0900

    Sieve of Eratosthenes / Divide and Conquer / Permutation Backtracking
    코드 조금 더 깔끔하게 수정. 유용하고 재밌는 문제다.

--- Algorithm/BOJ/3671.cpp
+++ Algorithm/BOJ/3671.cpp
@@ -1,34 +1,24 @@
 //https://www.acmicpc.net/problem/3671
 //http://1ilsang.blog.me/221366753276
 
+#include <iostream>
 #include <cstdio>
 #include <string>
 #include <cstring>
-#include <iostream>
 #include <vector>
 using namespace std;
 
-vector<int> v, chk, ord;
-int ans;
-const int MAX = 10000001;
+vector<int> v, list;
+const int MAX = 10000002;
 bool era[MAX];
-int chkNum[MAX];
-
-void getEratosthenes() {
-	era[0] = era[1] = true;
-	for (int i = 2; i * i <= MAX; i++) {
-		if (era[i] == false) {
-			for (int j = i + i; j <= MAX; j += i) {
-				era[j] = true;
-			}
-		}
-	}
-}
+bool chk[8];
+bool chkNum[MAX];
+int ans;
 
 int myPow(int n) {
 	if (n == 0) return 1;
 	int tmp = myPow(n / 2);
-	int ret = tmp * tmp;
+	int ret = tmp*tmp;
 	if (n % 2) ret *= 10;
 	return ret;
 }
@@ -36,21 +26,32 @@ int myPow(int n) {
 void go(int lo) {
 	if (lo > 0) {
 		int num = 0;
-		for (int i = 0; i < ord.size(); i++) {
-			num += v[ord[i]] * myPow(ord.size() - i - 1);
+		for (int i = 0; i < list.size(); i++) {
+			num += v[list[i]] * myPow(list.size() - i - 1);
 		}
-		if (chkNum[num] == 0) {
-			chkNum[num] = 1;
-			if (era[num] == false) ans++;
+		if (era[num] == false && chkNum[num] == false) {
+			chkNum[num] = true;
+			ans++;
 		}
 	}
 	for (int i = 0; i < v.size(); i++) {
 		if (chk[i] == 0) {
 			chk[i] = 1;
-			ord.push_back(i);
+			list.push_back(i);
 			go(lo + 1);
 			chk[i] = 0;
-			ord.pop_back();
+			list.pop_back();
+		}
+	}
+}
+
+void getEratos() {
+	era[0] = era[1] = true;
+	for (int i = 2; i < MAX; i++) {
+		if (era[i] == false) {
+			for (int j = i + i; j < MAX; j += i) {
+				era[j] = true;
+			}
 		}
 	}
 }
@@ -58,24 +59,19 @@ void go(int lo) {
 int main(void) {
 	int tcase;
 	scanf("%d", &tcase);
-	getEratosthenes();
+	getEratos();
 	for (int t = 0; t < tcase; t++) {
 		string str;
 		cin >> str;
-		chk.resize(str.size());
-
 		for (int i = 0; i < str.size(); i++) {
 			v.push_back(str[i] - 48);
 		}
-
 		go(0);
 		printf("%d\n", ans);
-
-		ans = 0;
-		v.clear();
-		chk.clear();
-		ord.clear();
+		memset(chk, 0, sizeof(chk));
 		memset(chkNum, 0, sizeof(chkNum));
+		v.clear();
+		ans = 0;
 	}
 	return 0;
 }
