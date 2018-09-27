5b092217e678d72fae9ea2709da939aa9cc1718d
LEE SANG-CHUL <1ilsang@naver.com>
2018-09-27 20:28:48 +0900

    Create 5988.cpp

---
+++ Algorithm/BOJ/5988.cpp
@@ -0,0 +1,19 @@
+//
+//
+
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		string tmp;
+		cin >> tmp;
+		if (tmp[tmp.size() - 1] % 2 == 0) printf("even\n");
+		else printf("odd\n");
+	}
+	return 0;
+}
