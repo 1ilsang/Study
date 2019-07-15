c3f6ceafa32f9fa9ee1e2e4bfe2f8210f15a7740
1ilsang <1ilsang@naver.com>
2019-07-15 23:59:48 +0900

    DP

---
+++ Algorithm/leetcode/96.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/unique-binary-search-trees/
+//https://1ilsang.blog.me/221586576648
+
+class Solution {
+    public int numTrees(int n) {
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j <= i; j++)
+				dp[i] += dp[j - 1] * dp[i - j];
+		}
+		return dp[n];
+    }
+}
