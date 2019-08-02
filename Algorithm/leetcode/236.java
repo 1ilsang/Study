7069a3e5f00bcce5746c4af729f6ed90c8f70d58
1ilsang <1ilsang@naver.com>
2019-08-03 01:24:35 +0900

    tree

---
+++ Algorithm/leetcode/236.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+//https://1ilsang.blog.me/221603869786
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == q || root == p) return root;
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if(left == null) return right;
+        else if(right == null) return left;
+        else return root;
+    }
+}
